d5b1aa73e437fe18c90befeee8f472811fce019a
Ezequiel <ezequiel3434@gmail.com>
2020-07-18 20:33:00 -0300

    mass Converter done

--- UnitConverter/Controller/ViewController.swift
+++ UnitConverter/Controller/ViewController.swift
@@ -14,9 +14,9 @@ class ViewController: UIViewController {
     var kgBtn = false
 
     @IBOutlet weak var txtField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var txtField2: UITextField!
     @IBOutlet weak var txtField3: UITextField!
-    @IBOutlet weak var txt4Field: UITextField!
+    @IBOutlet weak var txtField4: UITextField!
     @IBOutlet weak var btn1: CustomUIButton!
     @IBOutlet weak var btn2: CustomUIButton!
     @IBOutlet weak var btn3: CustomUIButton!
@@ -24,30 +24,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         txtField1.textAlignment = .center
-        textField2.textAlignment = .center
+        txtField2.textAlignment = .center
         txtField3.textAlignment = .center
-        txt4Field.textAlignment = .center
+        txtField4.textAlignment = .center
       
         btn1.customize(tittle: "Mile")
         btn2.customize(tittle: "Kilometer")
+        btn3.customize(tittle: "Pound")
+        btn4.customize(tittle: "Kilogram")
         
    
         
         
         let calcBtn = customCalculate()
-        
         calcBtn.addTarget(self, action: #selector(ViewController.txtField1Action), for: .touchUpInside)
 
         
         let calcBtn2 = customCalculate()
-        
         calcBtn2.addTarget(self, action: #selector(ViewController.txtField2Action), for: .touchUpInside)
         
+        let calcBtn3 = customCalculate()
+        calcBtn3.addTarget(self, action: #selector(ViewController.txtField3Action), for: .touchUpInside)
+        
+        let calcBtn4 = customCalculate()
+        calcBtn4.addTarget(self, action: #selector(ViewController.txtField4Action), for: .touchUpInside)
+        
+        
         txtField1.inputAccessoryView = calcBtn
-        textField2.inputAccessoryView = calcBtn2
-//        txtField3.inputAccessoryView = calcBtn2
-//        txt4Field.inputAccessoryView = calcBtn2
-//
+        txtField2.inputAccessoryView = calcBtn2
+        txtField3.inputAccessoryView = calcBtn3
+        txtField4.inputAccessoryView = calcBtn4
+
         
         
     }
@@ -57,8 +64,8 @@ class ViewController: UIViewController {
         let aux = txtField1.text
         
         
-        txtField1.text = textField2.text
-        textField2.text = aux
+        txtField1.text = txtField2.text
+        txtField2.text = aux
         
         let auxBtn = btn1.titleLabel!.text!
 
@@ -72,10 +79,10 @@ class ViewController: UIViewController {
     
     @IBAction func btnAction2(_ sender: Any) {
         
-        let aux = textField2.text
+        let aux = txtField2.text
         
         
-        textField2.text = txtField1.text
+        txtField2.text = txtField1.text
         txtField1.text = aux
         
         let auxBtn = btn2.titleLabel!.text!
@@ -86,18 +93,41 @@ class ViewController: UIViewController {
         
     }
     @IBAction func btnAction3(_ sender: Any) {
+        
+        let aux = txtField3.text
+        
+        
+        txtField3.text = txtField4.text
+        txtField4.text = aux
+        
+        let auxBtn = btn3.titleLabel!.text!
+
+        btn3.customize(tittle: btn4!.titleLabel!.text!)
+        btn4.customize(tittle: auxBtn)
     }
     @IBAction func btnAction4(_ sender: Any) {
+        
+        let aux = txtField4.text
+               
+               
+               txtField4.text = txtField3.text
+               txtField3.text = aux
+               
+               let auxBtn = btn4.titleLabel!.text!
+
+               btn4.customize(tittle: btn3!.titleLabel!.text!)
+               btn3.customize(tittle: auxBtn)
+               
     }
     
     @objc func txtField1Action() {
         if btn1.titleLabel?.text == "Mile" {
             print("Es mile")
-            textField2.text = mileToKm(mile: txtField1.text)
+            txtField2.text = mileToKm(mile: txtField1.text)
         } else {
             
             print("Es km")
-            textField2.text = kmToMile(km: txtField1.text)
+            txtField2.text = kmToMile(km: txtField1.text)
         }
     }
     
@@ -105,12 +135,35 @@ class ViewController: UIViewController {
     @objc func txtField2Action() {
            if btn2.titleLabel?.text == "Kilometer" {
              print("Es km")
-            txtField1.text = kmToMile(km: textField2.text)
+            txtField1.text = kmToMile(km: txtField2.text)
            } else {
-            txtField1.text = mileToKm(mile: textField2.text)
+            txtField1.text = mileToKm(mile: txtField2.text)
            }
        }
     
+    @objc func txtField3Action() {
+              if btn3.titleLabel?.text == "Pound" {
+                
+                txtField4.text = poundToKilogram(pound: txtField3.text)
+              } else {
+                txtField4.text = kilogramToPounds(kilo: txtField3.text)
+              }
+          }
+    
+    
+    @objc func txtField4Action() {
+                 if btn4.titleLabel?.text == "Kilogram" {
+                   
+                    txtField3.text = kilogramToPounds(kilo: txtField4.text)
+                 } else {
+                    txtField3.text = poundToKilogram(pound: txtField4.text)
+                 }
+             }
+    
+    
+    
+    
+    // MilesToKm
 
     func mileToKm(mile: String?) -> String {
         print("estoy en el 1")
@@ -128,7 +181,7 @@ class ViewController: UIViewController {
         return ""
     }
     
-   
+   // KmToMile
     
     func kmToMile(km: String?) -> String{
         print("Estoy en el 2")
@@ -146,6 +199,43 @@ class ViewController: UIViewController {
         }
         return ""
     }
+    
+    // PoundToKilogram
+    
+    func poundToKilogram(pound: String?) -> String {
+           
+           
+           if let txt = pound {
+                      if let pounds = Double(txt){
+                          view.endEditing(true)
+                       
+                          
+                        let distanceMiles = Measurement(value: pounds, unit: UnitMass.pounds)
+                          
+                          return String(format: "%.4f", (distanceMiles.converted(to: UnitMass.kilograms).value))
+                      }
+                  }
+           return ""
+       }
+    
+    
+    // KilogramToPounds
+    
+    func kilogramToPounds(kilo: String?) -> String {
+           
+           
+           if let txt = kilo {
+                      if let kilograms = Double(txt){
+                          view.endEditing(true)
+                       
+                          
+                        let distanceMiles = Measurement(value: kilograms, unit: UnitMass.kilograms)
+                          
+                          return String(format: "%.4f", (distanceMiles.converted(to: UnitMass.pounds).value))
+                      }
+                  }
+           return ""
+       }
     
 
     
